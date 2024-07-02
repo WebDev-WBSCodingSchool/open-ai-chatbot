5b089098a81153c546ce8aa2c70286acde2024b4
Jorge Paul <jserranopaul@gmail.com>
2024-07-02 13:13:58 +0200

    Path resolution

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  plugins: [react()]
+});
